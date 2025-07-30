d7dbcd9d3e8afc831ef4def83fb6595e1d4dea67
RussoAntonio96 <1996.antonio.russo@gmail.com>
2025-07-30 20:49:14 +0200

    feat(navbar):creazione navbar

---
+++ miriam-pedagogista/src/app/pages/homepage/homepage.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Navbar } from '../../components/navbar/navbar';
+
+@Component({
+  selector: 'app-homepage',
+  imports: [
+    Navbar
+  ],
+  templateUrl: './homepage.html',
+  styleUrl: './homepage.css'
+})
+export class Homepage {
+
+}
